a09d703fb6049339cef94a6d821425f6ff1217d6
Ming Li <ming.li@yunify.nl>
2019-04-22 12:50:00 +0200

    a

--- DartBoardApi/DartBoardApi/Controllers/ValuesController.cs
+++ DartBoardApi/DartBoardApi/Controllers/ValuesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DartBoardApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
