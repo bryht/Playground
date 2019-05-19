a0d236b1c8e76385b3fdfaa91038ac3412ced3d6
Ming Li <ming.li@yunify.nl>
2019-05-19 16:52:01 +0200

    test

--- src/DartBoardApi/Controllers/ValuesController.cs
+++ src/DartBoardApi/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace DartBoardApi.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value5" };
+            return new string[] { "value6" };
         }
 
         // GET api/values/5
