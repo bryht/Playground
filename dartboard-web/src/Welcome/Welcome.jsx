a28a30b30911e2a20300e4ec91a023b47c8e48d0
Ming Li <ming.li@yunify.nl>
2019-05-23 22:54:51 +0200

    style welcome page

---
+++ dartboard-web/src/Welcome/Welcome.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from '../logo.svg';
+import './Welcome.css';
+
+function Welcome() {
+  return (
+    <div className="body">
+      <header>
+        <img src={logo} className="logo" alt="logo" />
+        <p>
+          Dart Board
+        </p>
+      </header>
+      <div>
+        <label htmlFor="room">Your Name</label>
+        <input type="text" name="name" id="name" />
+        <label htmlFor="room">Your Code</label>
+        <input type="text" name="code" id="code" />
+      </div>
+      <div>
+        <button type="submit">Go</button>
+      </div>
+    </div>
+  );
+}
+
+export default Welcome;
